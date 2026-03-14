bd54e82165036c90e3264ce2824d81a42bf86070
Adams-Oluoch <joluoch@byupathway.edu>
2026-03-14 23:51:15 +0300

    exe3w1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        string playAgain = "yes";
+        
+        while (playAgain.ToLower() == "yes")
+        {
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("\nI'm thinking of a number between 1 and 100...");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+            
+            Console.Write("\nDo you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
