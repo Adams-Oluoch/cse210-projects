99121906e7f4fe1be1c4cab7fb706c09aa30d21c
Adams-Oluoch <joluoch@byupathway.edu>
2026-03-14 23:22:59 +0300

    exe2 w1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string letterGrade = "";
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Do not give up! Better luck next time!");
+        } 
+
     }
 }
